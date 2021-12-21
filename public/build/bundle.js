9653837ec4854524edf1976c3001870f9a3acf02
Rohit Karthik <rohit.karthik200@gmail.com>
2021-12-21 14:05:46 -0800

    add files

---
+++ public/build/bundle.js
@@ -0,0 +1,507 @@
+
+(function(l, r) { if (!l || l.getElementById('livereloadscript')) return; r = l.createElement('script'); r.async = 1; r.src = '//' + (self.location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1'; r.id = 'livereloadscript'; l.getElementsByTagName('head')[0].appendChild(r) })(self.document);
+var app = (function () {
+    'use strict';
+
+    function noop() { }
+    function add_location(element, file, line, column, char) {
+        element.__svelte_meta = {
+            loc: { file, line, column, char }
+        };
+    }
+    function run(fn) {
+        return fn();
+    }
+    function blank_object() {
+        return Object.create(null);
+    }
+    function run_all(fns) {
+        fns.forEach(run);
+    }
+    function is_function(thing) {
+        return typeof thing === 'function';
+    }
+    function safe_not_equal(a, b) {
+        return a != a ? b == b : a !== b || ((a && typeof a === 'object') || typeof a === 'function');
+    }
+    let src_url_equal_anchor;
+    function src_url_equal(element_src, url) {
+        if (!src_url_equal_anchor) {
+            src_url_equal_anchor = document.createElement('a');
+        }
+        src_url_equal_anchor.href = url;
+        return element_src === src_url_equal_anchor.href;
+    }
+    function is_empty(obj) {
+        return Object.keys(obj).length === 0;
+    }
+    function append(target, node) {
+        target.appendChild(node);
+    }
+    function insert(target, node, anchor) {
+        target.insertBefore(node, anchor || null);
+    }
+    function detach(node) {
+        node.parentNode.removeChild(node);
+    }
+    function element(name) {
+        return document.createElement(name);
+    }
+    function text(data) {
+        return document.createTextNode(data);
+    }
+    function space() {
+        return text(' ');
+    }
+    function attr(node, attribute, value) {
+        if (value == null)
+            node.removeAttribute(attribute);
+        else if (node.getAttribute(attribute) !== value)
+            node.setAttribute(attribute, value);
+    }
+    function children(element) {
+        return Array.from(element.childNodes);
+    }
+    function set_style(node, key, value, important) {
+        node.style.setProperty(key, value, important ? 'important' : '');
+    }
+    function custom_event(type, detail, bubbles = false) {
+        const e = document.createEvent('CustomEvent');
+        e.initCustomEvent(type, bubbles, false, detail);
+        return e;
+    }
+
+    let current_component;
+    function set_current_component(component) {
+        current_component = component;
+    }
+
+    const dirty_components = [];
+    const binding_callbacks = [];
+    const render_callbacks = [];
+    const flush_callbacks = [];
+    const resolved_promise = Promise.resolve();
+    let update_scheduled = false;
+    function schedule_update() {
+        if (!update_scheduled) {
+            update_scheduled = true;
+            resolved_promise.then(flush);
+        }
+    }
+    function add_render_callback(fn) {
+        render_callbacks.push(fn);
+    }
+    // flush() calls callbacks in this order:
+    // 1. All beforeUpdate callbacks, in order: parents before children
+    // 2. All bind:this callbacks, in reverse order: children before parents.
+    // 3. All afterUpdate callbacks, in order: parents before children. EXCEPT
+    //    for afterUpdates called during the initial onMount, which are called in
+    //    reverse order: children before parents.
+    // Since callbacks might update component values, which could trigger another
+    // call to flush(), the following steps guard against this:
+    // 1. During beforeUpdate, any updated components will be added to the
+    //    dirty_components array and will cause a reentrant call to flush(). Because
+    //    the flush index is kept outside the function, the reentrant call will pick
+    //    up where the earlier call left off and go through all dirty components. The
+    //    current_component value is saved and restored so that the reentrant call will
+    //    not interfere with the "parent" flush() call.
+    // 2. bind:this callbacks cannot trigger new flush() calls.
+    // 3. During afterUpdate, any updated components will NOT have their afterUpdate
+    //    callback called a second time; the seen_callbacks set, outside the flush()
+    //    function, guarantees this behavior.
+    const seen_callbacks = new Set();
+    let flushidx = 0; // Do *not* move this inside the flush() function
+    function flush() {
+        const saved_component = current_component;
+        do {
+            // first, call beforeUpdate functions
+            // and update components
+            while (flushidx < dirty_components.length) {
+                const component = dirty_components[flushidx];
+                flushidx++;
+                set_current_component(component);
+                update(component.$$);
+            }
+            set_current_component(null);
+            dirty_components.length = 0;
+            flushidx = 0;
+            while (binding_callbacks.length)
+                binding_callbacks.pop()();
+            // then, once components are updated, call
+            // afterUpdate functions. This may cause
+            // subsequent updates...
+            for (let i = 0; i < render_callbacks.length; i += 1) {
+                const callback = render_callbacks[i];
+                if (!seen_callbacks.has(callback)) {
+                    // ...so guard against infinite loops
+                    seen_callbacks.add(callback);
+                    callback();
+                }
+            }
+            render_callbacks.length = 0;
+        } while (dirty_components.length);
+        while (flush_callbacks.length) {
+            flush_callbacks.pop()();
+        }
+        update_scheduled = false;
+        seen_callbacks.clear();
+        set_current_component(saved_component);
+    }
+    function update($$) {
+        if ($$.fragment !== null) {
+            $$.update();
+            run_all($$.before_update);
+            const dirty = $$.dirty;
+            $$.dirty = [-1];
+            $$.fragment && $$.fragment.p($$.ctx, dirty);
+            $$.after_update.forEach(add_render_callback);
+        }
+    }
+    const outroing = new Set();
+    function transition_in(block, local) {
+        if (block && block.i) {
+            outroing.delete(block);
+            block.i(local);
+        }
+    }
+    function mount_component(component, target, anchor, customElement) {
+        const { fragment, on_mount, on_destroy, after_update } = component.$$;
+        fragment && fragment.m(target, anchor);
+        if (!customElement) {
+            // onMount happens before the initial afterUpdate
+            add_render_callback(() => {
+                const new_on_destroy = on_mount.map(run).filter(is_function);
+                if (on_destroy) {
+                    on_destroy.push(...new_on_destroy);
+                }
+                else {
+                    // Edge case - component was destroyed immediately,
+                    // most likely as a result of a binding initialising
+                    run_all(new_on_destroy);
+                }
+                component.$$.on_mount = [];
+            });
+        }
+        after_update.forEach(add_render_callback);
+    }
+    function destroy_component(component, detaching) {
+        const $$ = component.$$;
+        if ($$.fragment !== null) {
+            run_all($$.on_destroy);
+            $$.fragment && $$.fragment.d(detaching);
+            // TODO null out other refs, including component.$$ (but need to
+            // preserve final state?)
+            $$.on_destroy = $$.fragment = null;
+            $$.ctx = [];
+        }
+    }
+    function make_dirty(component, i) {
+        if (component.$$.dirty[0] === -1) {
+            dirty_components.push(component);
+            schedule_update();
+            component.$$.dirty.fill(0);
+        }
+        component.$$.dirty[(i / 31) | 0] |= (1 << (i % 31));
+    }
+    function init(component, options, instance, create_fragment, not_equal, props, append_styles, dirty = [-1]) {
+        const parent_component = current_component;
+        set_current_component(component);
+        const $$ = component.$$ = {
+            fragment: null,
+            ctx: null,
+            // state
+            props,
+            update: noop,
+            not_equal,
+            bound: blank_object(),
+            // lifecycle
+            on_mount: [],
+            on_destroy: [],
+            on_disconnect: [],
+            before_update: [],
+            after_update: [],
+            context: new Map(options.context || (parent_component ? parent_component.$$.context : [])),
+            // everything else
+            callbacks: blank_object(),
+            dirty,
+            skip_bound: false,
+            root: options.target || parent_component.$$.root
+        };
+        append_styles && append_styles($$.root);
+        let ready = false;
+        $$.ctx = instance
+            ? instance(component, options.props || {}, (i, ret, ...rest) => {
+                const value = rest.length ? rest[0] : ret;
+                if ($$.ctx && not_equal($$.ctx[i], $$.ctx[i] = value)) {
+                    if (!$$.skip_bound && $$.bound[i])
+                        $$.bound[i](value);
+                    if (ready)
+                        make_dirty(component, i);
+                }
+                return ret;
+            })
+            : [];
+        $$.update();
+        ready = true;
+        run_all($$.before_update);
+        // `false` as a special case of no DOM component
+        $$.fragment = create_fragment ? create_fragment($$.ctx) : false;
+        if (options.target) {
+            if (options.hydrate) {
+                const nodes = children(options.target);
+                // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+                $$.fragment && $$.fragment.l(nodes);
+                nodes.forEach(detach);
+            }
+            else {
+                // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+                $$.fragment && $$.fragment.c();
+            }
+            if (options.intro)
+                transition_in(component.$$.fragment);
+            mount_component(component, options.target, options.anchor, options.customElement);
+            flush();
+        }
+        set_current_component(parent_component);
+    }
+    /**
+     * Base class for Svelte components. Used when dev=false.
+     */
+    class SvelteComponent {
+        $destroy() {
+            destroy_component(this, 1);
+            this.$destroy = noop;
+        }
+        $on(type, callback) {
+            const callbacks = (this.$$.callbacks[type] || (this.$$.callbacks[type] = []));
+            callbacks.push(callback);
+            return () => {
+                const index = callbacks.indexOf(callback);
+                if (index !== -1)
+                    callbacks.splice(index, 1);
+            };
+        }
+        $set($$props) {
+            if (this.$$set && !is_empty($$props)) {
+                this.$$.skip_bound = true;
+                this.$$set($$props);
+                this.$$.skip_bound = false;
+            }
+        }
+    }
+
+    function dispatch_dev(type, detail) {
+        document.dispatchEvent(custom_event(type, Object.assign({ version: '3.44.3' }, detail), true));
+    }
+    function append_dev(target, node) {
+        dispatch_dev('SvelteDOMInsert', { target, node });
+        append(target, node);
+    }
+    function insert_dev(target, node, anchor) {
+        dispatch_dev('SvelteDOMInsert', { target, node, anchor });
+        insert(target, node, anchor);
+    }
+    function detach_dev(node) {
+        dispatch_dev('SvelteDOMRemove', { node });
+        detach(node);
+    }
+    function attr_dev(node, attribute, value) {
+        attr(node, attribute, value);
+        if (value == null)
+            dispatch_dev('SvelteDOMRemoveAttribute', { node, attribute });
+        else
+            dispatch_dev('SvelteDOMSetAttribute', { node, attribute, value });
+    }
+    function validate_slots(name, slot, keys) {
+        for (const slot_key of Object.keys(slot)) {
+            if (!~keys.indexOf(slot_key)) {
+                console.warn(`<${name}> received an unexpected slot "${slot_key}".`);
+            }
+        }
+    }
+    /**
+     * Base class for Svelte components with some minor dev-enhancements. Used when dev=true.
+     */
+    class SvelteComponentDev extends SvelteComponent {
+        constructor(options) {
+            if (!options || (!options.target && !options.$$inline)) {
+                throw new Error("'target' is a required option");
+            }
+            super();
+        }
+        $destroy() {
+            super.$destroy();
+            this.$destroy = () => {
+                console.warn('Component was already destroyed'); // eslint-disable-line no-console
+            };
+        }
+        $capture_state() { }
+        $inject_state() { }
+    }
+
+    /* src/App.svelte generated by Svelte v3.44.3 */
+
+    const file = "src/App.svelte";
+
+    function create_fragment(ctx) {
+    	let parent;
+    	let img0;
+    	let img0_src_value;
+    	let t0;
+    	let h10;
+    	let t2;
+    	let main0;
+    	let a0;
+    	let t4;
+    	let a1;
+    	let t6;
+    	let main1;
+    	let h11;
+    	let t7;
+    	let u;
+    	let t9;
+    	let t10;
+    	let img1;
+    	let img1_src_value;
+
+    	const block = {
+    		c: function create() {
+    			parent = element("parent");
+    			img0 = element("img");
+    			t0 = space();
+    			h10 = element("h1");
+    			h10.textContent = "The best timer and stopwatch app out there.";
+    			t2 = space();
+    			main0 = element("main");
+    			a0 = element("a");
+    			a0.textContent = "Download for Apple Silicon";
+    			t4 = space();
+    			a1 = element("a");
+    			a1.textContent = "Download for Windows";
+    			t6 = space();
+    			main1 = element("main");
+    			h11 = element("h1");
+    			t7 = text("A ");
+    			u = element("u");
+    			u.textContent = "beautiful";
+    			t9 = text(" looking UI.");
+    			t10 = space();
+    			img1 = element("img");
+    			set_style(img0, "padding", "0.5em");
+    			if (!src_url_equal(img0.src, img0_src_value = "icon.png")) attr_dev(img0, "src", img0_src_value);
+    			attr_dev(img0, "alt", "Timer and Stopwatch Icon");
+    			attr_dev(img0, "width", "200px");
+    			attr_dev(img0, "height", "200px");
+    			add_location(img0, file, 1, 1, 10);
+    			set_style(h10, "color", "#C0D5FB");
+    			set_style(h10, "font-family", "-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif");
+    			set_style(h10, "font-size", "100px");
+    			set_style(h10, "text-align", "center");
+    			add_location(h10, file, 8, 1, 128);
+    			attr_dev(a0, "class", "downloadLink svelte-bdbzdv");
+    			attr_dev(a0, "href", "Timer and Stopwatch-1.0.0-arm64.dmg");
+    			attr_dev(a0, "download", "");
+    			add_location(a0, file, 18, 2, 412);
+    			attr_dev(a1, "class", "downloadLinkWin svelte-bdbzdv");
+    			attr_dev(a1, "href", "Timer and Stopwatch Setup 1.0.0.exe");
+    			attr_dev(a1, "download", "");
+    			add_location(a1, file, 23, 2, 533);
+    			attr_dev(main0, "class", "svelte-bdbzdv");
+    			add_location(main0, file, 17, 1, 403);
+    			add_location(u, file, 38, 5, 930);
+    			set_style(h11, "color", "#C0D5FB");
+    			set_style(h11, "font-family", "-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif");
+    			set_style(h11, "font-size", "75px");
+    			set_style(h11, "width", "400px");
+    			set_style(h11, "margin", "0");
+    			add_location(h11, file, 30, 2, 695);
+    			set_style(img1, "padding", "2em");
+    			if (!src_url_equal(img1.src, img1_src_value = "screenshot1.png")) attr_dev(img1, "src", img1_src_value);
+    			attr_dev(img1, "alt", "Timer and Stopwatch Icon");
+    			attr_dev(img1, "width", "768");
+    			attr_dev(img1, "height", "475");
+    			add_location(img1, file, 40, 2, 970);
+    			set_style(main1, "padding-top", "8rem");
+    			attr_dev(main1, "class", "svelte-bdbzdv");
+    			add_location(main1, file, 29, 1, 659);
+    			attr_dev(parent, "class", "svelte-bdbzdv");
+    			add_location(parent, file, 0, 0, 0);
+    		},
+    		l: function claim(nodes) {
+    			throw new Error("options.hydrate only works if the component was compiled with the `hydratable: true` option");
+    		},
+    		m: function mount(target, anchor) {
+    			insert_dev(target, parent, anchor);
+    			append_dev(parent, img0);
+    			append_dev(parent, t0);
+    			append_dev(parent, h10);
+    			append_dev(parent, t2);
+    			append_dev(parent, main0);
+    			append_dev(main0, a0);
+    			append_dev(main0, t4);
+    			append_dev(main0, a1);
+    			append_dev(parent, t6);
+    			append_dev(parent, main1);
+    			append_dev(main1, h11);
+    			append_dev(h11, t7);
+    			append_dev(h11, u);
+    			append_dev(h11, t9);
+    			append_dev(main1, t10);
+    			append_dev(main1, img1);
+    		},
+    		p: noop,
+    		i: noop,
+    		o: noop,
+    		d: function destroy(detaching) {
+    			if (detaching) detach_dev(parent);
+    		}
+    	};
+
+    	dispatch_dev("SvelteRegisterBlock", {
+    		block,
+    		id: create_fragment.name,
+    		type: "component",
+    		source: "",
+    		ctx
+    	});
+
+    	return block;
+    }
+
+    function instance($$self, $$props) {
+    	let { $$slots: slots = {}, $$scope } = $$props;
+    	validate_slots('App', slots, []);
+    	const writable_props = [];
+
+    	Object.keys($$props).forEach(key => {
+    		if (!~writable_props.indexOf(key) && key.slice(0, 2) !== '$$' && key !== 'slot') console.warn(`<App> was created with unknown prop '${key}'`);
+    	});
+
+    	return [];
+    }
+
+    class App extends SvelteComponentDev {
+    	constructor(options) {
+    		super(options);
+    		init(this, options, instance, create_fragment, safe_not_equal, {});
+
+    		dispatch_dev("SvelteRegisterComponent", {
+    			component: this,
+    			tagName: "App",
+    			options,
+    			id: create_fragment.name
+    		});
+    	}
+    }
+
+    const app = new App({
+    	target: document.body,
+    	props: {
+    		name: 'world'
+    	}
+    });
+
+    return app;
+
+})();
+//# sourceMappingURL=bundle.js.map
---
+++ src/App.svelte
@@ -0,0 +1,96 @@
+<parent>
+	<img
+		style="padding: 0.5em"
+		src="icon.png"
+		alt="Timer and Stopwatch Icon"
+		width="200px"
+		height="200px"
+	/>
+	<h1
+		style="
+			color:#C0D5FB; 
+			font-family:-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+			font-size: 100px;
+			text-align: center;"
+	>
+		The best timer and stopwatch app out there.
+	</h1>
+	<main>
+		<a
+			class="downloadLink"
+			href="Timer and Stopwatch-1.0.0-arm64.dmg"
+			download>Download for Apple Silicon</a
+		>
+		<a
+			class="downloadLinkWin"
+			href="Timer and Stopwatch Setup 1.0.0.exe"
+			download>Download for Windows</a
+		>
+	</main>
+	<main style="padding-top: 8rem;">
+		<h1
+			style="
+			color:#C0D5FB; 
+			font-family:-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+			font-size: 75px;
+			width: 400px;
+			margin: 0;"
+		>
+			A <u>beautiful</u> looking UI. 
+		</h1>
+		<img
+			style="padding: 2em"
+			src="screenshot1.png"
+			alt="Timer and Stopwatch Icon"
+			width="768"
+			height="475"
+		/>
+	</main>
+</parent>
+
+<style>
+	parent {
+		padding: 0;
+		margin: 0;
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+	}
+	main {
+		display: flex;
+		flex-direction: row;
+		justify-content: center;
+		align-items: center;
+	}
+	.downloadLink {
+		color: white;
+		background-color: cornflowerblue;
+		padding: 0.9em;
+		border-radius: 0.4em;
+		margin: 0.4em;
+		transition: all 0.15s;
+	}
+	.downloadLinkWin {
+		color: white;
+		background-color: red;
+		padding: 0.9em;
+		border-radius: 0.4em;
+		margin: 0.4em;
+		transition: all 0.15s;
+	}
+	.downloadLink:hover {
+		text-decoration: none;
+		background-color: transparent;
+		padding: 0.825em 0.9em;
+		border: 1px solid cornflowerblue;
+		color: cornflowerblue;
+	}
+	.downloadLinkWin:hover {
+		text-decoration: none;
+		background-color: transparent;
+		padding: 0.825em 0.9em;
+		border: 1px solid red;
+		color: red;
+	}
+</style>
